05674fcfb6f61270672bab13fe167d85cd2ea99f
Matthew Pierce <mtpierce@live.com>
2019-11-27 20:57:34 -0500

    Toggel Waypoint
    Added features to toggle on/off waypoint visability in game view

---
+++ Assets/Scripts/ToggleWaypointVisibility.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleWaypointVisibility : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void FindAllWaypoints()
+    {
+        var waypointObjects = GameObject.FindGameObjectsWithTag("Waypoint");
+
+        Debug.Log("Found waypoints: " + waypointObjects.Length);
+
+        foreach(var waypoint in waypointObjects)
+        {
+            Debug.Log("Waypoint found: " + waypoint.name);
+        }
+
+        Debug.Log("Done.");
+    }
+
+    public void ToggleVisibility()
+    {
+        var waypointObjects = GameObject.FindGameObjectsWithTag("Waypoint");
+
+        for (int i = 0; i < waypointObjects.Length; i++)
+        {
+            GameObject waypoint = waypointObjects[i];
+            Toggle(waypoint.GetComponent<Renderer>());
+
+            //Deal with children
+            foreach(Transform child in waypoint.transform)
+            {
+                Toggle(child.GetComponent<Renderer>());
+            }
+        }
+
+    }
+
+    private static void Toggle(Renderer waypointRenderer)
+    {
+        if (waypointRenderer.enabled == false)
+        {
+            waypointRenderer.enabled = true;
+        }
+        else
+        {
+            waypointRenderer.enabled = false;
+        }
+    }
+}
